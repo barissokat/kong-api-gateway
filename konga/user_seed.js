dabdc81855cf375cd600810323d9b56d19289a5f
Barış Sokat <barissokat@outlook.com>
2021-06-07 10:10:44 +0300

    Initial commit

---
+++ konga/user_seed.js
@@ -0,0 +1,21 @@
+module.exports = [{
+        "username": "admin",
+        "email": "admin@user.com",
+        "firstName": "Admin",
+        "lastName": "User",
+        "node_id": "http://kong:8001",
+        "admin": true,
+        "active": true,
+        "password": "password"
+    },
+    {
+        "username": "demo",
+        "email": "demo@user.com",
+        "firstName": "Demo",
+        "lastName": "User",
+        "node_id": "http://kong:8001",
+        "admin": false,
+        "active": true,
+        "password": "password"
+    }
+]
